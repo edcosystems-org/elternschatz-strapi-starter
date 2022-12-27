8e5096cd9ad6b3eee6c99bc1fa722a6bc466befa
Tobias Feitkenhauer <tobias.feitkenhauer@edcosystems.com>
2022-12-27 16:40:58 +0100

    Initial Commit

---
+++ template/src/api/treasure/controllers/treasure.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * treasure controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::treasure.treasure');
---
+++ template/src/api/treasure/routes/treasure.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * treasure router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::treasure.treasure');
